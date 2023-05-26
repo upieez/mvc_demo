8eb2e45973778e9e5a31db9b53a5116337b3cb00
Samuel Huang <56812343+upieez@users.noreply.github.com>
2023-05-26 22:01:41 +0800

    add build and start script

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,7 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../../config/database.js")[env];
 const initProduct = require("./product.js");
