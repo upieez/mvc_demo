b0424b43807d58dc4eb76236686d859216f68db4
Samuel Huang <56812343+upieez@users.noreply.github.com>
2023-05-30 20:37:36 +0800

    remove sudo

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 npm install
 echo "Successfully ran npm install"
-sudo timedatectl set-timezone Asia/Singapore 
+timedatectl set-timezone Asia/Singapore 
 npx sequelize-cli db:create
 npx sequelize-cli db:migrate
